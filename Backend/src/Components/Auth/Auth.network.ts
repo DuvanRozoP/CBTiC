622912a23dd3c1fbc0892e9019b85e2d7248a32c
Duvanrozop <rozoparraduvan@gmail.com>
2023-03-21 03:25:32 -0500

    new endpoints

--- Backend/src/Components/Auth/Auth.network.ts
+++ Backend/src/Components/Auth/Auth.network.ts
@@ -1,22 +1,62 @@
-import express, { Request, Response } from 'express';
+import { Request, Response, Router } from 'express';
 
 // * CONTROLLER
-import { Login } from './Auth.controller';
+import {
+  signupController,
+  signinController,
+  getUserController,
+} from './Auth.controller';
 
 // * HELPER RESPONSE
-import { successResponse, errorResponse } from '../../Helpers/response';
-const router = express.Router();
+import {
+  successResponse,
+  successWithHeader,
+  errorResponse,
+} from '../../Helpers/response';
+import { tokenValidation } from '../../libs/validateToken';
 
-router.post('/', async (request: Request, response: Response) => {
+const router = Router();
+
+router.post('/signup', async (request: Request, response: Response) => {
   try {
-    const { user, password } = request.body;
-    successResponse(request, response, 200, await Login(user, password));
-  } catch (error: any) {
-    const arrayMessage = error.message.split('-');
-    const status = Number(arrayMessage[0]);
-    const Message = arrayMessage[1];
-    errorResponse(request, response, status, Message);
+    successWithHeader(request, response, await signupController(request.body));
+  } catch (error: unknown) {
+    if (typeof error === 'object' && error !== null && 'message' in error) {
+      const errorMessage = (error as Error).message;
+      errorResponse(request, response, errorMessage.split('-'));
+    }
   }
 });
+
+router.post('/signin', async (request: Request, response: Response) => {
+  try {
+    successWithHeader(request, response, await signinController(request.body));
+  } catch (error: unknown) {
+    if (typeof error === 'object' && error !== null && 'message' in error) {
+      const errorMessage = (error as Error).message;
+      errorResponse(request, response, errorMessage.split('-'));
+    }
+  }
+});
+
+router.get(
+  '/user',
+  tokenValidation,
+  async (request: Request, response: Response) => {
+    try {
+      successResponse(
+        request,
+        response,
+        200,
+        await getUserController(request.userId)
+      );
+    } catch (error: unknown) {
+      if (typeof error === 'object' && error !== null && 'message' in error) {
+        const errorMessage = (error as Error).message;
+        errorResponse(request, response, errorMessage.split('-'));
+      }
+    }
+  }
+);
 
 export default router;
