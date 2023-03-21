6c63133d969130496c40256d2875c7fd5e7dad20
Duvanrozop <rozoparraduvan@gmail.com>
2023-03-21 03:15:00 -0500

    new response of the endpoints

--- Backend/src/Helpers/response.ts
+++ Backend/src/Helpers/response.ts
@@ -1,23 +1,51 @@
 import { Request, Response } from 'express';
+import { ObjectId } from 'mongoose';
+
+export interface IUser {
+  _id: ObjectId;
+  user: string;
+  password: string;
+  __v: number;
+}
+
+export interface HeaderSuccess {
+  status: number;
+  header: string;
+  token: string;
+  messageSuccess: string;
+}
 
 export function successResponse(
   _req: Request,
   res: Response,
   status: number,
-  message: string | Object
+  message: string | IUser[]
 ) {
   res.status(status || 200).send({
     success: message,
   });
 }
 
+export function successWithHeader(
+  _req: Request,
+  res: Response,
+  message: HeaderSuccess
+) {
+  const { status, header, token, messageSuccess } = message;
+  res
+    .status(status || 200)
+    .header(header, token)
+    .send({
+      success: messageSuccess,
+    });
+}
+
 export function errorResponse(
   _req: Request,
   res: Response,
-  status: number,
-  message: string
+  errorFatal: string[]
 ) {
-  res.status(status || 401).send({
-    error: message,
+  res.status(Number(errorFatal[0]) || 401).send({
+    error: errorFatal[1],
   });
 }
