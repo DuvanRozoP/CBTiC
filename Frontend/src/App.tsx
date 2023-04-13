2fee583a44de01b843015e018f04acdd65010917
Daniel Montillo <Danielmontillo55@gmail.com>
2023-04-13 11:52:34 -0500

    Navbar update

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -6,8 +6,11 @@ import './App.css';
 import Teacher from './Pages/Teacher/Teacher';
 import Landing from './Pages/Landing/Landing';
 import Nabvar from './Pages/Nabvar/Nabvar';
+import DetailSubjets from './Pages/DetailSubjets/DetailSubjets';
+import Teachers from './Pages/Teachers/Teachers';
+import Login from './Pages/Login/Login';
 // & COMPONENTS
-import Loading from './Components/Loading/Loading';
+// import Loading from './Components/Loading/Loading';
 
 function App() {
   const router = createBrowserRouter([
@@ -16,13 +19,25 @@ function App() {
       element: <Nabvar />,
       children: [
         {
-          path: 'home',
+          path: 'login',
+          element: <Login/>
+        },
+        {
+          path: 'Inicio',
           element: <Landing />,
         },
         {
-          path: 'subjects',
+          path: 'Asignaturas',
           element: <Teacher />,
         },
+        {
+          path: 'detailsubjets',
+          element: <DetailSubjets />
+        },
+        {
+          path: 'Profesores',
+          element: <Teachers />
+        }
       ],
     },
   ]);
